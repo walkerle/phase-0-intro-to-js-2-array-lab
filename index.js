71f9e7b2a6d07cd5bd2d53c621c76dcd688ebd6a
walkerle <118040893+walkerle@users.noreply.github.com>
2022-11-18 14:49:33 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-// Write your solution here!
+const cats = [
+    "Milo",
+    "Otis",
+    "Garfield",
+]
+
+function destructivelyAppendCat(name) {
+    cats.push(name);
+    return;
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+    return;
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+    return;
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+    return;
+}
+
+function appendCat(name) {
+    return [...cats, name];
+}
+
+function prependCat(name) {
+    return [name, ...cats];
+}
+
+function removeLastCat() {
+    return cats.slice(0, cats.length - 1);
+}
+
+function removeFirstCat() {
+    return cats.slice(1);
+}
